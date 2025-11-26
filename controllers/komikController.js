3eb9600adeaad19c5a43345fb8cb3ed9c5d81429
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-11-26 14:24:47 +0700

    create komik baru & simpan gambar bila ada upload

---
+++ controllers/komikController.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+const komikService = require("../services/komikService");
+
+async function createKomik(req, res) {
+  try {
+    const komikData = req.body;
+
+    if (req.file) {
+      komikData.ImageType = req.file.mimetype;
+      komikData.ImageName = req.file.originalname;
+      komikData.ImageData = req.file.buffer;
+    }
+
+    const result = await komikService.createKomik(db, komikData);
+    res.status(201).json({ success: true, data: result });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+}
